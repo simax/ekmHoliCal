ce0d9c3f0b9be869d1a5773fcea64dd9d6a0f23d
Simon Lomax <simax.99.99@gmail.com>
2012-05-10 06:58:11 +0100

    Attempting to add install/uninstall mongo as service batch files

---
+++ Install as service.bat
@@ -0,0 +1,2 @@
+c:\mongodb\bin\mongod --install --serviceName "ekmHoliCalDB" --serviceDisplayName "ekmHolicalDB" --serviceDescription "MongoDB data for ekmHoliCal" --logpath "c:\inetpub\wwwroot\ekmHoliCal\data\log.txt" --logappend --dbpath "c:\inetpub\wwwroot\ekmholical\data\ekmHoliCal" --port 8120 --serviceUser "simon" --servicePassword "megan_ellie"
+pause
